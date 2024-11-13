2c26bf6f655e679560c0a39e67a452201a41e263
d8gohel <98903536+d8gohel@users.noreply.github.com>
2024-11-13 15:28:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+# import 
+model= pickle.load(open("spam.pkl",'rb'))
+cv=pickle.load(open("vectorizer.pkl",'rb'))
+st.title("Spam check")
+st.write("THis is ml")
+user_input=st.text_area("mail desctiption",height=150)
+if st.button("classify"):
+    
+    if user_input:
+        data= [user_input]
+        vect= cv.transform(data).toarray()
+        pred= model.predict(vect)
+        if pred[0]==0:
+            st.write("not spam")
+            st.success("not spam")
+        else:
+
+            st.write("spam")
+            st.error("this is spam email")
+    else:
+        print("try agin")
+    
